833af3e521307b80f271d1b09030ca9afbc9c700
Krishna <k-soni@ispace-inc.com>
2022-06-22 17:17:13 +0900

    changed the read to automatically extract data

--- experimentPlot.py
+++ experimentPlot.py
@@ -21,47 +21,64 @@ class manipulate_data:
             
             if self._flag['quantity'] == 'force':
                 data_quantity = 'leptrino_force_torque_on_wheel-force_torque.csv'
+                filename = f"../data/{self._date}/{data_type}/{self._date}_{self._sr}/{data_quantity}"
+                self._df = pd.read_csv(filename, usecols=['time', '.wrench.force.x', '.wrench.force.z'])
+
             elif self._flag['quantity'] == 'sinkage':
-                data_quantity = 'swt_driver-vertical_unit_log'
+                data_quantity = 'swt_driver-vertical_unit_log.csv'
+                filename = f"../data/{self._date}/{data_type}/{self._date}_{self._sr}/{data_quantity}"
+                self._df = pd.read_csv(filename, usecols=['time', '.wheel_sinkage'])
+                
             
-            filename = f"../data/{self._date}/{data_type}/{self._date}_{self._sr}/{data_quantity}"
-            self._df = pd.read_csv(filename)
-        
         elif self._flag['type'] == 'simulation':
             data_type = 'simulationData'
             
             if self._flag['quantity'] == 'force':
                 data_quantity = 'result_monitor.csv'
+                filename = f"../data/{self._date}/{data_type}/{self._date}_{self._sr}/{data_quantity}"
+                self._df = pd.read_csv(filename, header=1, usecols=['Time', 'wheel.fx', 'wheel.fz'])
+
             elif self._flag['quantity'] == 'sinkage':
                 data_quantity = 'CenterOfMass.txt'
+                filename = f"../data/{self._date}/{data_type}/{self._date}_{self._sr}/{data_quantity}"
+                self._df = pd.read_csv(filename, sep=' ', names=['Time', 'Sinkage'], skiprows=[0])
             
-            filename = f"../data/{self._date}/{data_type}/{self._date}_{self._sr}/{data_quantity}"
-            self._df = pd.read_csv(filename, skiprows=[0])
+        #print(self._df)
         
 
     def _extract_data(self):
         """extract the data from the csv based on the flag values
         """
-
+        self._read_data()
         if self._flag['type'] == 'experiment':
-            self._df = self._df[["time", ".wrench.force.x", ".wrench.force.z"]]
-            self._df['fx/fz'] = self._df[".wrench.force.x"]/self._df[".wrench.force.z"]
-            self._df['time'] = pd.to_datetime(self._df['time'])
-            self._df.rename(columns={'time': 'Time'}, inplace=True)
-            #self._df['timeCalculated'] = self._df['Time'] - self._df.loc[0, 'Time']
-            #for x in self._df.index:
-                #self._df.loc[x, 'timeCalculated'] = self._df.loc[x, 'timeCalculated'].seconds
+            if self._flag['quantity'] == 'force':
+                self._df['fx/fz'] = self._df['.wrench.force.x']/self._df['.wrench.force.z']
+                self._df['time'] = pd.to_datetime(self._df['time'])
+                self._df.rename(columns={'time': 'Time'}, inplace=True)
+                self._df['timeCalculated'] = self._df['Time'] - self._df.loc[0, 'Time']
+                for x in self._df.index:
+                    self._df.loc[x, 'timeCalculated'] = self._df.loc[x, 'timeCalculated'].microseconds
+            
+            elif self._flag['quantity'] == 'sinkage':
+                self._df['time'] = pd.to_datetime(self._df['time'])
+                self._df.rename(columns={'time': 'Time'}, inplace=True)
+                #print(self._df)
+        
         
         elif self._flag['type'] == 'simulation':
-            self._df = self._df[["Time", "wheel.fx", "wheel.fz"]]
-            self._df['fx/fz'] = self._df["wheel.fx"]/self._df["wheel.fz"]
-            self._df['fx/fz'].fillna(0, inplace=True)
-            self._df['Time'] = self._df['Time'] - self._df.loc[0, 'Time']
-            for x in self._df.index:
-                if not -20 <= self._df.loc[x, 'fx/fz'] <=20:
-                    self._df.drop(x, inplace=True)
-        
+            if self._flag['quantity'] == 'force':
+                self._df['fx/fz'] = self._df["wheel.fx"]/self._df["wheel.fz"]
+                self._df['fx/fz'].fillna(0, inplace=True)
+                self._df['Time'] = self._df['Time'] - self._df.loc[0, 'Time']
+                for x in self._df.index:
+                    if not -20 <= self._df.loc[x, 'fx/fz'] <=20:
+                        self._df.drop(x, inplace=True)
+            
+            elif self._flag['quantity'] == 'sinkage':
+                self._df['Time'] = self._df['Time'] - self._df.loc[0, 'Time']
+                
         print(self._df)
+        print(self._df['Time'])
 
     def plot_data(self):
         """plotting the data
@@ -77,11 +94,15 @@ class manipulate_data:
 if __name__ == '__main__':
     
     date, sr = 20220615, 10
-    data1_values = {'type': 'experiment', 'quantity': 'force'}
+    """
+    data1_values = {'type': 'experiment', 'quantity': 'sinkage'}
     data1 = manipulate_data(date, sr, data1_values)
-    data1.plot_data()
-    
+    data1._extract_data()
 
     data2_values = {'type': 'simulation', 'quantity': 'force'}
     data2 = manipulate_data(date, sr, data2_values)
-    data2.plot_data()
+    data2._read_data()
+    """
+    data_values = {'type': 'experiment', 'quantity': 'force'}
+    data = manipulate_data(date, sr, data_values)
+    data._read_data()
