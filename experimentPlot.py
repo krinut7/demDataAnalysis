1048cb70db180e1c9751f1972820b2e35cd7d7c4
Krishna <k-soni@ispace-inc.com>
2022-06-22 18:12:59 +0900

    changed the time column

--- experimentPlot.py
+++ experimentPlot.py
@@ -11,7 +11,8 @@ class manipulate_data:
         self._flag = flag
         self._date = date
         self._sr = sr
-        
+    
+    @property
     def _read_data(self):
         """creates the filename based on the flags and then reads the data into df dataFrame
         """
@@ -45,11 +46,11 @@ class manipulate_data:
             
         #print(self._df)
         
-
-    def _extract_data(self):
+    @property
+    def clean_data(self):
         """extract the data from the csv based on the flag values
         """
-        self._read_data()
+        self._read_data
         if self._flag['type'] == 'experiment':
             if self._flag['quantity'] == 'force':
                 self._df['fx/fz'] = self._df['.wrench.force.x']/self._df['.wrench.force.z']
@@ -57,7 +58,8 @@ class manipulate_data:
                 self._df.rename(columns={'time': 'Time'}, inplace=True)
                 self._df['timeCalculated'] = self._df['Time'] - self._df.loc[0, 'Time']
                 for x in self._df.index:
-                    self._df.loc[x, 'timeCalculated'] = self._df.loc[x, 'timeCalculated'].microseconds
+                    self._df.loc[x, 'Time'] = self._df.loc[x, 'timeCalculated'].seconds + self._df.loc[x, 'timeCalculated'].microseconds/1000000
+                #self._df = self._df.groupby(['timeCalculated']).mean().reset_index()
             
             elif self._flag['quantity'] == 'sinkage':
                 self._df['time'] = pd.to_datetime(self._df['time'])
@@ -77,32 +79,22 @@ class manipulate_data:
             elif self._flag['quantity'] == 'sinkage':
                 self._df['Time'] = self._df['Time'] - self._df.loc[0, 'Time']
                 
-        print(self._df)
-        print(self._df['Time'])
+        return self._df
 
-    def plot_data(self):
-        """plotting the data
-        """
-        self._read_data()
-        self._extract_data()
-        #self._df.plot()
-        plt.plot(self._df['Time'], self._df['fx/fz'], '-')
-        plt.show()
+class plot_data:
+    """Class to plot the data
+    """
+    
 
 
 
 if __name__ == '__main__':
     
     date, sr = 20220615, 10
-    """
-    data1_values = {'type': 'experiment', 'quantity': 'sinkage'}
-    data1 = manipulate_data(date, sr, data1_values)
-    data1._extract_data()
 
-    data2_values = {'type': 'simulation', 'quantity': 'force'}
-    data2 = manipulate_data(date, sr, data2_values)
-    data2._read_data()
-    """
     data_values = {'type': 'experiment', 'quantity': 'force'}
     data = manipulate_data(date, sr, data_values)
-    data._read_data()
+    dataFrame1 = data.clean_data
+    data.plot_data()
+    print(dataFrame1)
+    #print(type(dataFrame1.loc[0, 'timeCalculated']))
