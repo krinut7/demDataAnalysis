88dd181212ccc3c8524a4ef0dce6b6a4f871e769
Krishna <k-soni@ispace-inc.com>
2022-06-23 15:55:53 +0900

    added plotting function. Need to check the data

--- experimentPlot.py
+++ experimentPlot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
 class data_frame:
     """Class to read, clean, and extract only required data
@@ -51,12 +52,14 @@ class data_frame:
         self._read_data
         if self._flag['type'] == 'experiment':
             if self._flag['quantity'] == 'force':
-                self._df['fx/fz'] = self._df['.wrench.force.x']/self._df['.wrench.force.z']
+                self._df['Fx/Fz'] = self._df['.wrench.force.x']/self._df['.wrench.force.z']
                 self._df['time'] = pd.to_datetime(self._df['time'])
                 self._df['time'] = self._df['time'] - self._df.loc[0, 'time']
                 for x in self._df.index:
                     self._df.loc[x, 'time'] = self._df.loc[x, 'time'].seconds + self._df.loc[x, 'time'].microseconds/1000000
-                self._df.rename(columns={'time': 'Time'}, inplace=True)
+                    if not -0.3 <= self._df.loc[x, 'Fx/Fz'] <=0.3:
+                        self._df.drop(x, inplace=True)
+                self._df.rename(columns={'.wrench.force.x': 'Fx', '.wrench.force.z': 'Fz', 'time': 'Time'}, inplace=True)
                 #self._df = self._df.groupby(['time']).mean().reset_index()
             
             elif self._flag['quantity'] == 'sinkage':
@@ -64,37 +67,76 @@ class data_frame:
                 self._df['time'] = self._df['time'] - self._df.loc[0, 'time']
                 for x in self._df.index:
                     self._df.loc[x, 'time'] = self._df.loc[x, 'time'].seconds + self._df.loc[x, 'time'].microseconds/1000000
-                self._df.rename(columns={'time': 'Time'}, inplace=True)
+                _ = list(range(self._df.index.size - 5, self._df.index.size))
+                self._df.drop(_, inplace=True)
+                self._df.rename(columns={'time': 'Time', '.wheel_sinkage': 'Sinkage'}, inplace=True)
         
         elif self._flag['type'] == 'simulation':
             if self._flag['quantity'] == 'force':
-                self._df['fx/fz'] = self._df["wheel.fx"]/self._df["wheel.fz"]
-                self._df['fx/fz'].fillna(0, inplace=True)
+                self._df['Fx/Fz'] = self._df["wheel.fx"]/self._df["wheel.fz"]
+                self._df['Fx/Fz'].fillna(0, inplace=True)
                 self._df['Time'] = self._df['Time'] - self._df.loc[0, 'Time']
                 for x in self._df.index:
-                    if not -20 <= self._df.loc[x, 'fx/fz'] <=20:
+                    if not -5 <= self._df.loc[x, 'Fx/Fz'] <=5:
                         self._df.drop(x, inplace=True)
+                self._df.rename(columns={'wheel.fx': 'Fx', 'wheel.fz': 'Fz'}, inplace=True)
             
             elif self._flag['quantity'] == 'sinkage':
                 self._df['Time'] = self._df['Time'] - self._df.loc[0, 'Time']
                 
         return self._df
-
-class plot_data:
-    """Class to plot the data
-    """
-    def __init__(self, df):
-        self._df = df
-        print(self._df)
+    
+    def plot_data(self):
+        self._df = self.clean_data
+        if self._flag['quantity'] == 'force':
+                plt.plot('Time', 'Fx/Fz', data=self._df, linestyle='-')
+        elif self._flag['quantity'] == 'sinkage':
+            plt.plot('Time', 'Sinkage', data=self._df, linestyle='-')
+        
+        plt.show()
+    
+    def plot_data_compare(self, data=None):
+        _ = 10
+        fig, ax = plt.subplots()
+        
+        for i in range(0,5):
+            if self._flag['type'] == 'experiment':
+                if self._flag['quantity'] == 'force':
+                    ax.set(xlabel='Time', ylabel='Fx/Fz', title='Fx/Fz Experiment', autoscale_on=True, xlim=(0,20))
+                    ax.plot('Time', 'Fx/Fz', data=data[i], linestyle='-', label=f'SR{_}')
+                elif self._flag['quantity'] == 'sinkage':
+                    ax.set(xlabel='Time', ylabel='Sinkage', title='Sinkage Experiment', autoscale_on=True)
+                    ax.plot('Time', 'Sinkage', data=data[i], linestyle='-', label=f'SR{_}')
+            
+            elif self._flag['type'] == 'simulation':
+                if self._flag['quantity'] == 'force':
+                    ax.set(xlabel='Time', ylabel='Fx/Fz', title='Fx/Fz Simulation', autoscale_on=True, xlim=(0,2))
+                    ax.plot('Time', 'Fx/Fz', data=data[i], linestyle='-', label=f'SR{_}')
+                elif self._flag['quantity'] == 'sinkage':
+                    ax.set(xlabel='Time', ylabel='Sinkage', title='Sinkage Simulation', autoscale_on=True)
+                    ax.plot('Time', 'Sinkage', data=data[i], linestyle='-', label=f'SR{_}')
+            _ = _ + 20
+        plt.legend()
+        plt.show()
+        #return cls
+        
 
 
 
 
 if __name__ == '__main__':
     
-    date, sr = 20220615, 10
-
-    data_values = {'type': 'experiment', 'quantity': 'sinkage'}
-    data = data_frame(date, sr, data_values)
-    dataFrame1 = data.clean_data
-    plot_data = plot_data(dataFrame1)
+    data_values = {'type': f'{sys.argv[1]}', 'quantity': f'{sys.argv[2]}'}
+    date = 20220615
+    SR = [10, 30, 50, 70, 90]
+    data = []
+    dataFrame = []
+    for i in range(0,5):
+        data.insert(i, data_frame(date, SR[i], data_values))
+        dataFrame.insert(i,data[i].clean_data)
+    
+    data[0].plot_data_compare(dataFrame)
+    #print(data[1].clean_data)
+    #data_frame.plot_data_compare(data[0],dataFrame)
+    
+    
