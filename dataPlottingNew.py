37957176818c87268e3784dab991e1b8a96260b1
Krishna <k-soni@ispace-inc.com>
2022-07-10 19:18:40 +0900

    Added the functions.Working now

--- dataPlottingNew.py
+++ dataPlottingNew.py
@@ -45,7 +45,7 @@ def exp_sinkage():
 
     df['time'] = pd.to_datetime(df['time'])
     df['time'] = df['time'] - df.loc[0, 'time']
-
+    df['.wheel_sinkage'] = df['.wheel_sinkage'] / 100000000
     for x in df.index:
         df.loc[x, 'time'] = (df.loc[x, 'time'].seconds
                              + df.loc[x, 'time'].microseconds
@@ -69,15 +69,22 @@ def sim_force():
     df = pd.read_csv(filename, header=1, usecols=[
         'Time', 'wheel.fx', 'wheel.fy', 'wheel.fz'])
 
+    for x in df.index:
+        if df.loc[x, 'Time'] < 2:
+            df = df.drop(x)
+        else:
+            break
+
+    print(x)
     # print(df)
     df['Fx/Fz'] = df["wheel.fx"] / df["wheel.fz"]
     # print(df)
     df['Fx/Fz'] = df['Fx/Fz'].fillna(0)
     # print(df)
-    df['Time'] = df['Time'] - df.loc[0, 'Time']
+    # df['Time'] = df['Time'] - df.loc[0, 'Time']
 
     # for x in df.index:
-    # if not -1 <= df.loc[x, 'Fx/Fz'] <= 1:  # removing extreme values
+    # if not -20 <= df.loc[x, 'wheel.fz'] <= 20:  # removing extreme values
     # df = df.drop(x)
 
     df = df.rename(columns={
@@ -106,27 +113,28 @@ def plot_data(df_exp=None, df_sim=None):
     if df_sim is None:
         df_sim = dict()
 
+    plot_value = 'Fx/Fz'
     fig, ax = plt.subplots(1, 2)
 
     ax[0].set(
-        xlabel='Time', ylabel='Fx/Fz', title=f'Fx/Fz: SR{SR}',
+        xlabel='Time', ylabel=plot_value, title=f'{plot_value}: SR{SR}',
         autoscale_on=True)
-    ax[0].plot(
+    """ax[0].plot(
         'Time', 'Fx', data=df_exp['force'], linestyle='-',
-        label='Experiment')
+        label='Experiment')"""
     ax[0].plot(
-        'Time', 'Fx', data=df_sim['force'], linestyle='-',
+        'Time', plot_value, data=df_sim['force'], linestyle='-',
         label='Simulation')
 
     ax[1].set(
         xlabel='Time', ylabel='Sinkage',
         title=f'Sinkage: SR{SR}', autoscale_on=True)
-    """ax[1].plot(
-        'Time', 'Sinkage', data=df_exp['sinkage'], linestyle='-',
-        label='Experiment')"""
     ax[1].plot(
+        'Time', 'Sinkage', data=df_exp['sinkage'], linestyle='-',
+        label='Experiment')
+    """ax[1].plot(
         'Time', 'Sinkage', data=df_sim['sinkage'], linestyle='-',
-        label='Simulation')
+        label='Simulation')"""
 
     ax[0].legend()
     ax[1].legend()
@@ -146,7 +154,7 @@ def main():
 
     # print(SR)
     # print(f'Experiment:\n{df_exp}\nSimulation:\n{df_sim}')
-    print(f"Force:\n{df_sim['force']}")
+    print(f"Force:\n{df_sim['force'].to_string()}")
     plot_data(df_exp=df_exp, df_sim=df_sim)
 
 
